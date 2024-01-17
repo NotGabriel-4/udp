36cd472c42d18834cb4bd9ec3993a8fa3c85e2ed
NotGabriel-4 <gabriel.gonzalezl@outlook.com>
2024-01-16 22:55:54 -0300

    Hola Mundo

---
+++ extra/java/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo { // Definición de clase, debe ser igual al nombre del archivo
+
+    public static void main(String[] args) { // Main o funcion principal
+        System.out.println("Hola mundo");
+    }
+
+}
